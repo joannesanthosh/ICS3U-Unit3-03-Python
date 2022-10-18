65a4f5ec659899444cd40666e4d1fe0ab1739e66
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-10-18 14:24:24 +0000

    update

--- guessing_game_1.py
+++ guessing_game_1.py
@@ -20,7 +20,7 @@ def main():
     if user_guess == random_number:
         print("Correct guess!")
     else:
-        print("Incorrect. The number was: ".format(random_number))
+        print("Incorrect. The number was: {0}.".format(random_number))
 
         print("\nDone.")
 
